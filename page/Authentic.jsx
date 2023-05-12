c284d2e74e44a746c52f8322705d8bde8e5dfb13
uckevin <115539187+uckevin@users.noreply.github.com>
2023-05-12 11:07:11 +0800

    新增AuthenticPage,public/assets
    增加認證頁面雛型與所有圖檔

---
+++ page/Authentic.jsx
@@ -0,0 +1,134 @@
+function AuthenticationPage(){
+    return(<div>
+        <div className="authentication-sign-in-a-container">
+        <div className="authentication-sign-in-a-authentication-sign-in-a">
+          <div className="authentication-sign-in-a-block">
+            <div className="authentication-sign-in-a-frame427319044">
+              <span className="authentication-sign-in-a-text H2">
+                <span>Sign In</span>
+              </span>
+              <span className="authentication-sign-in-a-text02 14Regular1">
+                <span>Your Social Campaigns</span>
+              </span>
+            </div>
+            <div className="authentication-sign-in-a-frame427318925">
+              <div className="authentication-sign-in-a-form">
+                <div className="authentication-sign-in-a-google">
+                  <div className="authentication-sign-in-a-google1">
+                    <div className="authentication-sign-in-a-superg">
+                      <img
+                        src="public/playground_assets/shapei339-7e79.svg"
+                        alt="ShapeI339"
+                        className="authentication-sign-in-a-shape"
+                      />
+                      <img
+                        src="public/playground_assets/shapei339-jfp7.svg"
+                        alt="ShapeI339"
+                        className="authentication-sign-in-a-shape1"
+                      />
+                      <img
+                        src="public/playground_assets/shapei339-xmvb.svg"
+                        alt="ShapeI339"
+                        className="authentication-sign-in-a-shape2"
+                      />
+                      <img
+                        src="public/playground_assets/shapei339-igx.svg"
+                        alt="ShapeI339"
+                        className="authentication-sign-in-a-shape3"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="authentication-sign-in-a-autoaddedframe">
+                  <span className="authentication-sign-in-a-text04 14Regular">
+                    <span>Sign in with Google</span>
+                  </span>
+                </div>
+              </div>
+              <div className="authentication-sign-in-a-form1">
+                <div className="authentication-sign-in-a-apple">
+                  <div className="authentication-sign-in-a-apple1">
+                    <img
+                      src="public/playground_assets/shapei339-0aop.svg"
+                      alt="ShapeI339"
+                      className="authentication-sign-in-a-shape4"
+                    />
+                  </div>
+                </div>
+                <div className="authentication-sign-in-a-autoaddedframe1">
+                  <span className="authentication-sign-in-a-text06 14Regular">
+                    <span>Sign in with Apple</span>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="authentication-sign-in-a-frame427318927">
+              <img
+                src="public/playground_assets/line236i339-e9r8.svg"
+                alt="Line236I339"
+                className="authentication-sign-in-a-line236"
+              />
+              <span className="authentication-sign-in-a-text08 12Regular">
+                <span>Or with Email</span>
+              </span>
+              <img
+                src="public/playground_assets/line237i339-z6b.svg"
+                alt="Line237I339"
+                className="authentication-sign-in-a-line237"
+              />
+            </div>
+            <div className="authentication-sign-in-a-frame427319054">
+              <div className="authentication-sign-in-a-form2">
+                <span className="authentication-sign-in-a-text10 14Regular">
+                  <span>Email</span>
+                </span>
+              </div>
+              <div className="authentication-sign-in-a-frame427319041">
+                <div className="authentication-sign-in-a-form3">
+                  <span className="authentication-sign-in-a-text12 14Regular">
+                    <span>Password</span>
+                  </span>
+                </div>
+                <span className="authentication-sign-in-a-text14 14Regular1">
+                  <span>Forgot Password?</span>
+                </span>
+              </div>
+            </div>
+            <div className="authentication-sign-in-a-frame427319042">
+              <button className="authentication-sign-in-a-button">
+                <span className="authentication-sign-in-a-text16 H31">
+                  <span>Sign In</span>
+                </span>
+              </button>
+              <span className="authentication-sign-in-a-text18 14Regular1">
+                <span className="authentication-sign-in-a-text19">
+                  Not a Member yet?
+                </span>
+                <span className="authentication-sign-in-a-text20"></span>
+                <span>Sign Up</span>
+              </span>
+            </div>
+          </div>
+          <div className="authentication-sign-in-a-header-b"></div>
+          <div className="authentication-sign-in-a-copyright">
+            <span className="authentication-sign-in-a-text22 12Regular1">
+              <span>© 2023 GO ECO</span>
+            </span>
+          </div>
+          <img
+            src="public/playground_assets/screenshot20230411at15262i339-cdje-200h.png"
+            alt="Screenshot20230411at15262I339"
+            className="authentication-sign-in-a-screenshot20230411at15262"
+          />
+          <img
+            src="public/playground_assets/screenshot20230412at03102i339-tjgg-200h.png"
+            alt="Screenshot20230412at03102I339"
+            className="authentication-sign-in-a-screenshot20230412at03102"
+          />
+        </div>
+      </div>
+
+    </div>
+    )
+}
+export default AuthenticationPage;
