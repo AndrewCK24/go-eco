d62c2fdf4372805cbd1d0b000b211ea3ceff2ba2
uckevin <115539187+uckevin@users.noreply.github.com>
2023-05-14 01:00:05 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AuthenticationPage from '../page/Authentic'
+import '/pageCSS/Authentic.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
