3fd142bc585465165a518fc954d1fcaedfb1a54b
uckevin <115539187+uckevin@users.noreply.github.com>
2023-05-13 20:48:03 +0800

    刪減Authentic.jsx code

--- page/Authentic.jsx
+++ page/Authentic.jsx
@@ -1,51 +1,34 @@
 import SignInButton from "../components/SignInButton";
 function AuthenticationPage(){
     return(<div>
-        <div className="container">
-        <div className="authentication-sign-in-a-authentication-sign-in-a">
-          <div className="authentication-sign-in-a-block">
+        <div className="page">
+          <div className="container">
             <div className="authentication-sign-in-a-frame427319044">
-              <span className="authentication-sign-in-a-text H2">
-                <span>Sign In</span>
-              </span>
-              <span className="authentication-sign-in-a-text02 14Regular1">
+              <span>Sign In</span>
+              <span className="authentication-sign-in-a-text02">
                 <span>Your Social Campaigns</span>
               </span>
             </div>
             <div className="authentication-sign-in-a-frame427318925">
               <div className="authentication-sign-in-a-form">
                 <div className="authentication-sign-in-a-autoaddedframe">
-                  <span className="authentication-sign-in-a-text04 14Regular">
-                    <SignInButton name="Sign in with Google"></SignInButton>
+                  <span className="authentication-sign-in-a-text04">
+                    <SignInButton src="" name="Sign in with Google"></SignInButton>
                   </span>
                 </div>
               </div>
               <div className="authentication-sign-in-a-form1">
-                <div className="authentication-sign-in-a-apple">
-                  <div className="authentication-sign-in-a-apple1">
-                  </div>
-                </div>
                 <div className="authentication-sign-in-a-autoaddedframe1">
                   <span className="authentication-sign-in-a-text06 14Regular">
-                    <SignInButton src="public/assets/shapei339-0aop.svg" name="Sign in with Apple"></SignInButton>
+                    <SignInButton src="" name="Sign in with Apple"></SignInButton>
                   </span>
                 </div>
               </div>
             </div>
             <div className="authentication-sign-in-a-frame427318927">
-              <img
-                src="public/assets/line236i339-e9r8.svg"
-                alt="Line236I339"
-                className="authentication-sign-in-a-line236"
-              />
               <span className="authentication-sign-in-a-text08 12Regular">
                 <span>Or with Email</span>
               </span>
-              <img
-                src="public/assets/line237i339-z6b.svg"
-                alt="Line237I339"
-                className="authentication-sign-in-a-line237"
-              />
             </div>
             <div className="authentication-sign-in-a-frame427319054">
               <div className="authentication-sign-in-a-form2">
@@ -60,7 +43,7 @@ function AuthenticationPage(){
                   </span>
                 </div>
                 <span className="authentication-sign-in-a-text14 14Regular1">
-                  <a>Forgot Password?</a>
+                  <a rel="#page">Forgot Password?</a>
                 </span>
               </div>
             </div>
@@ -71,7 +54,7 @@ function AuthenticationPage(){
                 </span>
               </button>
               <span className="authentication-sign-in-a-text18 14Regular1">
-                <a className="authentication-sign-in-a-text19">
+                <a rel="#page" className="authentication-sign-in-a-text19">
                   Not a Member yet?
                 </a>
                 <span className="authentication-sign-in-a-text20"></span>
@@ -86,16 +69,10 @@ function AuthenticationPage(){
             </span>
           </div>
           <img
-            src="public/assets/screenshot20230411at15262i339-cdje-200h.png"
-            alt="Screenshot20230411at15262I339"
-            className="authentication-sign-in-a-screenshot20230411at15262"
+            src="public/assets/LOGO_GOECO.png"
+            alt="LOGO"
+            className="LOGO"
           />
-          <img
-            src="public/assets/screenshot20230412at03102i339-tjgg-200h.png"
-            alt="Screenshot20230412at03102I339"
-            className="authentication-sign-in-a-screenshot20230412at03102"
-          />
-        </div>
         </div>
     </div>
     )
