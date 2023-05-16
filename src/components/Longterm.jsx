984f762f8f27e5c280b2cf164e40cba95e06b4d5
uckevin <115539187+uckevin@users.noreply.github.com>
2023-05-17 00:52:48 +0800

    修改EventCreatePage.css

--- src/components/Longterm.jsx
+++ src/components/Longterm.jsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
 function Longterm(){
     return(<Link path="/" className="ECP_BlueBlock">  
-        <div className="ECP_BBText1">Longterm project</div>
-        <div className="ECP_BBText2">For continuous</div>
+        <div className="ECP_BText1">Longterm project</div>
+        <div className="ECP_BText2">For continuous</div>
     </Link>
     )  
 }
--- src/components/Single.jsx
+++ src/components/Single.jsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
 function Single(){
     return(<Link path="/" className="ECP_GreenBlock">
-        <div className="ECP_GBText1">Single activity</div>
-        <div className="ECP_GBText2">For only one time</div> 
+        <div className="ECP_BText1">Single activity</div>
+        <div className="ECP_BText2">For only one time</div> 
     </Link>
     )
 }
