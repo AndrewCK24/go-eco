58446c26e5c74b087aad7df505d8ec63f9f9e4db
AndrewCK24 <andrew50412@gmail.com>
2023-05-17 20:36:56 +0800

    修改 main.jsx 排版，加入 BottomNav-1.jsx

---
+++ src/components/Nav/BottomNav-1.jsx
@@ -0,0 +1,74 @@
+import styled from "@emotion/styled";
+// import "../../../pageCSS/BottomNav.css";
+import BottomNavCol from "./BottomNavCol";
+
+const Footer = styled.div`
+  margin-top: 100%;
+  width: 100%;
+  height: 55%;
+  display: flex;
+  overflow: hidden;
+  position: absolute;
+  background-color: rgba(30, 30, 30, 1);
+`;
+
+const LeftFooter = styled.div``;
+
+const LeftFooterText = styled.div`
+  top: 110px;
+  left: 110px;
+  color: rgba(255, 255, 255, 1);
+  position: absolute;
+  font-size: larger;
+  font-weight: 700;
+`;
+
+const RightFooter = styled.div`
+  gap: 40px;
+  top: 10%;
+  right: 10%;
+  display: flex;
+  position: absolute;
+  background-color: rgba(30, 30, 30, 1);
+`;
+
+const BottomNav = () => {
+  return (
+    <Footer>
+      <LeftFooter>
+        <img src="/assets/LOGO_GOECO.png" className="F_Picture" />
+        {/* Icons */}
+        <LeftFooterText>
+          The earth is a fine place
+          <br />
+          and worth fighting for.
+        </LeftFooterText>
+      </LeftFooter>
+      <RightFooter>
+        <BottomNavCol
+          text1="Information"
+          text2="About"
+          text3="Product"
+          text4="Blog"
+        />
+        <BottomNavCol
+          text1="Company"
+          text2="Community"
+          text3="Career"
+          text4="Our story"
+        />
+        <BottomNavCol
+          text1="Contact"
+          text2="Getting Started"
+          text3="Pricing"
+          text4="Resources"
+        />
+        <div className="FooterText">
+          2023 all Right Reserved Term of use GO ECO
+        </div>
+      </RightFooter>
+    </Footer>
+  );
+};
+
+export default BottomNav;
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import NavBar from './components/Nav/NavBar'
-import BottomNav from './components/Nav/BottomNav'
-import HomePage from './HomePage'
-import LogInPage from './LogInPage'
-import ProposalPage from './ProposalPage'
-import { HashRouter, Route, Routes } from 'react-router-dom'
-import EventCreatePage from './EventCreatePage'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import NavBar from "./components/Nav/NavBar";
+import BottomNav from "./components/Nav/BottomNav";
+import HomePage from "./HomePage";
+import LogInPage from "./LogInPage";
+import ProposalPage from "./ProposalPage";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import EventCreatePage from "./EventCreatePage";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HashRouter>
-    <NavBar />
+      <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/LogInPage" element={<LogInPage />} />
         <Route path="/EventCreatePage" element={<EventCreatePage />} />
-        <Route path="/EventCreatePage/ProposalPage" element={<ProposalPage />} />
+        <Route
+          path="/EventCreatePage/ProposalPage"
+          element={<ProposalPage />}
+        />
       </Routes>
-    <BottomNav />
+      <BottomNav />
     </HashRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
