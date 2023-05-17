7d543e4e77e62569cda9b10ed567919f8b84618c
uckevin <115539187+uckevin@users.noreply.github.com>
2023-05-17 21:38:14 +0800

    修改main元件,新增App元件

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import NavBar from "./components/Nav/NavBar";
-import BottomNav from "./components/Nav/BottomNav";
-import HomePage from "./HomePage";
-import LogInPage from "./LogInPage";
-import ProposalPage from "./ProposalPage";
-import { HashRouter, Route, Routes } from "react-router-dom";
-import EventCreatePage from "./EventCreatePage";
+import App from "./app";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/LogInPage" element={<LogInPage />} />
-        <Route path="/EventCreatePage" element={<EventCreatePage />} />
-        <Route
-          path="/EventCreatePage/ProposalPage"
-          element={<ProposalPage />}
-        />
-      </Routes>
-      <BottomNav />
-    </HashRouter>
+   <App />
   </React.StrictMode>
 );
