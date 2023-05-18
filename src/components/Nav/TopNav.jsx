8f97d88e42668ddb3a75292ee3f2df8c3eaec355
uckevin <115539187+uckevin@users.noreply.github.com>
2023-05-19 00:50:17 +0800

    修改merge後程式碼

--- src/components/Nav/TopNav.jsx
+++ src/components/Nav/TopNav.jsx
@@ -1,58 +1,77 @@
 import { Link } from "react-router-dom";
 import styled from "@emotion/styled";
+// import Logo from "../../../public/assets/LOGO_GOECO.png";
+import { ReactComponent as Logo } from "../../icons/favicon.svg";
 import { ReactComponent as UserIcon } from "../../icons/userIcon.svg";
 
-const IconContainer = styled.div`
-  svg {
-    width: 1.5rem;
-    height: 1.5rem;
-  }
+const Container = styled.nav`
+	width: 100%;
+	display: grid;
+	grid-template-columns: auto 1fr auto;
+	grid-column-gap: 1rem;
+	align-items: center;
 `;
 
+const LogoContainer = styled.div`
+padding-left: 2rem;
+svg {
+  width: 12rem;
+  height: 3rem;
+}
+`;
+const LinkContainer = styled.div`
+	font-family: "Poppins", sans-serif;
+	gap: 1rem;
+	font-size: 1.25rem;
+	width: max-content;
+	display: flex;
+	flex-direction: row;
+`;
+
+const TabText = styled.div`
+	color: rgba(30, 30, 30, 1);
+	text-decoration: none;
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+	position: relative;
+`;
+
+
+const IconContainer = styled.div`
+  padding-right: 2rem;
+	display: flex;
+	height: auto;
+	right: 0%;
+  svg {
+    width: 2rem;
+    height: 2rem;
+  }
+`;
 const TopNav = () => {
-  const Nav = {
-    gap: "40px",
-    top: "2.5%",
-    width: "100%",
-    display: "flex",
-    position: "absolute",
-  };
-  const text = {
-    color: "var(0, 0, 0, 0.4000000059604645)",
-    textDecoration: "none",
-    display: "flex",
-    alignItems: "center",
-    flexDirection: "column",
-    position: "relative",
-  };
-  const LogIn = {
-    display: "flex",
-    width: "5%",
-    height: "auto",
-    right: "0%",
-    position: "absolute",
-  };
-  return (
-    <div style={Nav}>
-      <div>
-        <img src="../../assets/LOGO_GOECO.png" alt="logo" />
-      </div>
-      <Link to="/" style={text}>
-        Home
-      </Link>
-      <Link to="/EventCreatePage" style={text}>
-        Proposals
-      </Link>
-      <Link to="" style={text}>
-        Contacts    {/* TODO: wait for implememt */}
-      </Link>
-      <Link to="/LogInPage" style={LogIn}>
-        <IconContainer>
-          <UserIcon />
-        </IconContainer>
-      </Link>
-    </div>
-  );
+	return (
+		<Container>
+			<LogoContainer>
+				<Logo />
+			</LogoContainer>
+			<LinkContainer>
+				<Link to="/">
+					<TabText>Home</TabText>
+				</Link>
+				<Link to="/EventCreatePage">
+					<TabText>Proposals</TabText>
+				</Link>
+				<Link to="/">
+					<TabText>Contacts</TabText>
+				</Link>
+			</LinkContainer>
+			<Link to="/LogInPage">
+				<IconContainer>
+					<UserIcon />
+				</IconContainer>
+			</Link>
+		</Container>
+	);
 };
 
 export default TopNav;
