bb851c8cfeeb9f4918b95379b3a4650ab2e8d33a
AndrewCK24 <andrew50412@gmail.com>
2023-05-19 04:03:30 +0800

    切分元件 Hero (from HomePage)

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,5 +1,6 @@
 import styled from "@emotion/styled";
 import "../pageCSS/HomePage.css";
+import Hero from "./components/HomePage/Hero";
 import Recycle from "./components/HomePage/Recycle";
 import CleanUp from "./components/HomePage/CleanUp";
 
@@ -9,62 +10,6 @@ const Container = styled.div`
 	overflow: auto;
 	align-items: center;
 	flex-direction: column;
-`;
-
-const HeroContainer = styled.section`
-	width: 90%;
-	display: grid;
-	grid-template-columns: 1fr auto;
-	border-radius: 1.5rem;
-	background-color: rgba(179, 230, 193, 1);
-`;
-
-const HeroLeft = styled.div`
-	width: 100%;
-	margin: 5%;
-	display: grid;
-	grid-template-rows: 2fr 1fr 1fr;
-`;
-
-const HeroTitle = styled.div`
-	display: flex;
-	align-items: flex-end;
-	font-size: 4rem;
-	font-weight: 700;
-`;
-
-const HeroFigureContainer = styled.div`
-	display: grid;
-	grid-template-columns: auto 1fr;
-`;
-
-const HeroFigure = styled.div`
-	padding: 0 1.5rem 0 0;
-	display: grid;
-	grid-template-rows: 1fr auto;
-`;
-
-const HeroFigureNumber = styled.div`
-	display: flex;
-	align-items: flex-end;
-	font-size: 2.5rem;
-	font-weight: 700;
-`;
-
-const HeroFigureTitle = styled.div`
-	font-size: 1.25rem;
-`;
-
-const HeroRight = styled.div`
-	background-image: url("../public/assets/mainBlockBackGround.png");
-	background-size: 75%;
-	background-repeat: no-repeat;
-	background-position: bottom;
-	display: flex;
-	align-items: flex-end;
-	img {
-		height: 80%;
-	}
 `;
 
 const EventContainer = styled.section`
@@ -83,28 +28,7 @@ const FeedBackContainer = styled.section`
 const HomePage = () => {
 	return (
 		<Container>
-			<HeroContainer>
-				<HeroLeft>
-					<HeroTitle>Now’s the time to do something for our planet</HeroTitle>
-					<HeroFigureContainer>
-						<HeroFigure>
-							<HeroFigureNumber>500+</HeroFigureNumber>
-							<HeroFigureTitle>Beach cleanup</HeroFigureTitle>
-						</HeroFigure>
-						<HeroFigure>
-							<HeroFigureNumber>1000+</HeroFigureNumber>
-							<HeroFigureTitle>Eco activities</HeroFigureTitle>
-						</HeroFigure>
-					</HeroFigureContainer>
-					<div className="SearchBlock">
-						<div className="SearchBlockText">What are you looking for?</div>
-						<img src="/assets/search.svg" className="search" />
-					</div>
-				</HeroLeft>
-				<HeroRight>
-					<img src="../public/assets/potPlant.png" alt="PotPlant" />
-				</HeroRight>
-			</HeroContainer>
+			<Hero />
 			{/* <div className="MainBlock">
 				<div className="LeftBlock">
 					<div className="LeftBlockText">
---
+++ src/components/HomePage/Hero.jsx
@@ -0,0 +1,84 @@
+import styled from "@emotion/styled";
+
+const HeroContainer = styled.section`
+	width: 90%;
+	display: grid;
+	grid-template-columns: 1fr auto;
+	border-radius: 1.5rem;
+	background-color: rgba(179, 230, 193, 1);
+`;
+
+const HeroLeft = styled.div`
+	width: 100%;
+	margin: 5%;
+	display: grid;
+	grid-template-rows: 2fr 1fr 1fr;
+`;
+
+const HeroTitle = styled.div`
+	display: flex;
+	align-items: flex-end;
+	font-size: 4rem;
+	font-weight: 700;
+`;
+
+const HeroFigureContainer = styled.div`
+	display: grid;
+	grid-template-columns: auto 1fr;
+`;
+
+const HeroFigure = styled.div`
+	padding: 0 1.5rem 0 0;
+	display: grid;
+	grid-template-rows: 1fr auto;
+`;
+
+const HeroFigureNumber = styled.div`
+	display: flex;
+	align-items: flex-end;
+	font-size: 2.5rem;
+	font-weight: 700;
+`;
+
+const HeroFigureTitle = styled.div`
+	font-size: 1.25rem;
+`;
+
+const HeroRight = styled.div`
+	background-image: url("../public/assets/mainBlockBackGround.png");
+	background-size: 75%;
+	background-repeat: no-repeat;
+	background-position: bottom;
+	display: flex;
+	align-items: flex-end;
+	img {
+		height: 80%;
+	}
+`;
+
+const Hero = () => (
+	<HeroContainer>
+		<HeroLeft>
+			<HeroTitle>Now’s the time to do something for our planet</HeroTitle>
+			<HeroFigureContainer>
+				<HeroFigure>
+					<HeroFigureNumber>500+</HeroFigureNumber>
+					<HeroFigureTitle>Beach cleanup</HeroFigureTitle>
+				</HeroFigure>
+				<HeroFigure>
+					<HeroFigureNumber>1000+</HeroFigureNumber>
+					<HeroFigureTitle>Eco activities</HeroFigureTitle>
+				</HeroFigure>
+			</HeroFigureContainer>
+			<div className="SearchBlock">
+				<div className="SearchBlockText">What are you looking for?</div>
+				<img src="/assets/search.svg" className="search" />
+			</div>
+		</HeroLeft>
+		<HeroRight>
+			<img src="../public/assets/potPlant.png" alt="PotPlant" />
+		</HeroRight>
+	</HeroContainer>
+);
+
+export default Hero;
