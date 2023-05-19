0bab04f9931a119be30093a421f8e34fbb716cbc
AndrewCK24 <andrew50412@gmail.com>
2023-05-20 04:03:07 +0800

    修改 TopNav 新增 SearchBar

--- src/components/Nav/TopNav.jsx
+++ src/components/Nav/TopNav.jsx
@@ -3,6 +3,7 @@ import styled from "@emotion/styled";
 // import Logo from "../../../public/assets/LOGO_GOECO.png";
 import { ReactComponent as Logo } from "../../icons/favicon.svg";
 import { ReactComponent as UserIcon } from "../../icons/userIcon.svg";
+import { ReactComponent as SearchIcon } from "../../icons/search.svg";
 
 const Container = styled.nav`
 	width: 100%;
@@ -11,14 +12,19 @@ const Container = styled.nav`
 	grid-template-columns: auto 1fr auto;
 	grid-column-gap: 1rem;
 	align-items: center;
+	a {
+		display: flex;
+		align-items: center;
+		text-decoration: none;
+	}
 `;
 
 const LogoContainer = styled.div`
-padding-left: 2rem;
-svg {
-  width: 12rem;
-  height: 3rem;
-}
+	padding-left: 2rem;
+	svg {
+		width: 12rem;
+		height: 3rem;
+	}
 `;
 const LinkContainer = styled.div`
 	font-family: "Poppins", sans-serif;
@@ -30,25 +36,49 @@ const LinkContainer = styled.div`
 `;
 
 const TabText = styled.div`
-	color: rgba(30, 30, 30, 1);
-	text-decoration: none;
+	color: var(--primary-black);
 	display: flex;
 	align-items: center;
 	flex-direction: column;
 	position: relative;
 `;
 
-
-const IconContainer = styled.div`
-  padding-right: 2rem;
-	display: flex;
-	height: auto;
+const RightNav = styled.div`
 	right: 0%;
-  svg {
-    width: 2rem;
-    height: 2rem;
-  }
+	padding-right: 2rem;
+	display: flex;
+	gap: 0.5rem;
+	height: auto;
+	svg {
+		width: 2rem;
+		height: 2rem;
+	}
 `;
+
+const SearchContainer = styled.form`
+	display: grid;
+	grid-template-columns: 1fr auto;
+	background-color: var(--background-gray);
+	border-radius: 0.5rem;
+`;
+
+const SearchInput = styled.input`
+	border: none;
+	background-color: transparent;
+	border-radius: 0.5rem;
+`;
+
+const SearchBtn = styled.button`
+	border: none;
+	background-color: transparent;
+	display: flex;
+	align-items: center;
+	svg {
+		height: 1.5rem;
+		width: 1.5rem;
+	}
+`;
+
 const TopNav = () => {
 	return (
 		<Container>
@@ -66,11 +96,17 @@ const TopNav = () => {
 					<TabText>Contacts</TabText>
 				</Link>
 			</LinkContainer>
-			<Link to="/LogInPage">
-				<IconContainer>
+			<RightNav>
+				<SearchContainer>
+					<SearchInput />
+					<SearchBtn>
+						<SearchIcon />
+					</SearchBtn>
+				</SearchContainer>
+				<Link to="/LogInPage">
 					<UserIcon />
-				</IconContainer>
-			</Link>
+				</Link>
+			</RightNav>
 		</Container>
 	);
 };
