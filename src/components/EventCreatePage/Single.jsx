f6ac570879c8c0ae793b1a13a28faba3f55a2cfd
uckevin <115539187+uckevin@users.noreply.github.com>
2023-05-22 01:28:04 +0800

    EventCreatePage.jsx調整中

--- src/components/EventCreatePage/Single.jsx
+++ src/components/EventCreatePage/Single.jsx
@@ -1,35 +1,34 @@
+import styled from "@emotion/styled";
 import { Link } from "react-router-dom";
+
+const Text1 = styled.div`
+    display: 'flex';
+    font-size: '2rem';
+    font-weight: '700';
+    color: 'rgba(0, 0, 0, 1)';
+`;
+   
+const Text2 = styled.div`
+    display: 'flex';
+    font-size: '1.5rem';
+    font-weight: '400;
+    color: 'rgba(0, 0, 0, 1)';
+`;
+
 function Single(){
     const style={
         width: '100%',
-        height: '231.52268981933594rem',
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
-        overflow: 'hidden',
-        borderRadius: '15rem',
+        borderRadius: '1.5rem',
         backgroundColor: ' rgba(179, 230, 193, 0.5)',
         textDecoration: 'none'
     };
-    const text1={
-        top: '30%',
-        color: 'rgba(0, 0, 0, 1)',
-        position: 'relative',
-        display: 'flex',
-        fontSize: '32rem',
-        fontWeight: '700'
-    };
-    const text2={
-        top: '35%',
-        color: 'rgba(0, 0, 0, 1)',
-        position: 'relative',
-        display: 'flex',
-        fontSize: '24rem',
-        fontWeight: '400'
-    };
-    return(<Link to="/EventCreatePage/ProposalPage" style={style}>
-        <div style={text1}>Single activity</div>
-        <div style={text2}>For only one time</div> 
+    return(
+    <Link to="/EventCreatePage/ProposalPage" style={style}>
+        <Text1>activity</Text1>
+        <Text2>For only one time</Text2>
     </Link>
     )
 }
