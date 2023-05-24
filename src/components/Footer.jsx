eb1d6013c351b23bf17bea69c50dbadad7913800
AndrewCK24 <andrew50412@gmail.com>
2023-05-24 21:41:22 +0800

    變更 Footer 排版

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,30 @@
 import styled from "@emotion/styled";
-import BottomNavCol from "./Nav/BottomNavCol";
-import { ReactComponent as Logo } from "../icons/favicon.svg";
 
-const FooterContainer = styled.footer`
+import BottomNavCol from "./Nav/BottomNavCol";
+import Logo from "./Logo";
+
+const OuterContainer = styled.footer`
 	width: 100%;
+	height: auto;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background-color: var(--primary-black);
+`;
+
+const FooterContainer = styled.div`
+	width: 90%;
 	grid-row: 2 / 3;
 	display: grid;
+	grid-gap: 1rem;
 	grid-template-columns: 2fr 1fr;
 	grid-template-rows: 1fr auto;
 	font-size: 1rem;
 	overflow: hidden;
-	background-color: rgba(30, 30, 30, 1);
 `;
 
 const LeftFooter = styled.div`
-	padding: 1rem 2rem;
+	padding: 1rem 0;
 	grid-column: 1 / 2;
 	grid-row: 1 / 2;
 `;
@@ -24,15 +34,8 @@ const LeftFooterText = styled.div`
 	font-weight: 700;
 `;
 
-const IconContainer = styled.div`
-	svg {
-		width: 12rem;
-		height: 3rem;
-	}
-`;
-
 const RightFooter = styled.div`
-	padding: 1rem 2rem;
+	padding: 1rem 0;
 	grid-column: 2 / 3;
 	grid-row: 1 / 2;
 	display: grid;
@@ -45,7 +48,7 @@ const RightFooterRow = styled.div`
 `;
 
 const FooterText = styled.div`
-	padding: 1rem 2rem;
+	padding: 1rem 0;
 	grid-column: 1 / 3;
 	grid-row: 2 / 3;
 	color: rgba(217, 217, 217, 1);
@@ -54,39 +57,39 @@ const FooterText = styled.div`
 
 const Footer = () => {
 	return (
-		<FooterContainer>
-			<LeftFooter>
-				<IconContainer>
+		<OuterContainer>
+			<FooterContainer>
+				<LeftFooter>
 					<Logo />
-				</IconContainer>
-				<LeftFooterText>
-					The earth is a fine place
-					<br />
-					and worth fighting for.
-				</LeftFooterText>
-			</LeftFooter>
-			<RightFooter>
-				{/* <BottomNavCol
+					<LeftFooterText>
+						The earth is a fine place
+						<br />
+						and worth fighting for.
+					</LeftFooterText>
+				</LeftFooter>
+				<RightFooter>
+					{/* <BottomNavCol
 					text1="Information"
 					text2="About"
 					text3="Product"
 					text4="Blog"
-				/>
-				<BottomNavCol
+					/>
+					<BottomNavCol
 					text1="Company"
 					text2="Community"
 					text3="Career"
 					text4="Our story"
-				/>
-				<BottomNavCol
+					/>
+					<BottomNavCol
 					text1="Contact"
 					text2="Getting Started"
 					text3="Pricing"
 					text4="Resources"
 				/> */}
-			</RightFooter>
-			<FooterText>2023 all Right Reserved Term of use GO ECO</FooterText>
-		</FooterContainer>
+				</RightFooter>
+				<FooterText>2023 all Right Reserved Term of use GO ECO</FooterText>
+			</FooterContainer>
+		</OuterContainer>
 	);
 };
 
---
+++ src/components/Logo.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import styled from "@emotion/styled";
+
+import { ReactComponent as LogoIcon } from "../icons/favicon.svg";
+
+const Container = styled(Link)`
+	svg {
+		width: 20vw;
+		max-width: 12rem;
+		height: 3rem;
+	}
+`;
+
+const Logo = () => {
+  return (
+    <Container to="/">
+      <LogoIcon />
+    </Container>
+  );
+}
+
+export default Logo;
