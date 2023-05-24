c1be90a6974d69e8f10bb74f1b5224fc81cc62a9
AndrewCK24 <andrew50412@gmail.com>
2023-05-24 20:34:51 +0800

    修改 Event Join Now 按鈕

--- src/components/HomePage/Event.jsx
+++ src/components/HomePage/Event.jsx
@@ -6,8 +6,8 @@ const EventBox = styled.div`
 	flex-direction: column;
 	grid-template-rows: auto 1fr;
 	border-radius: 1rem;
-	background-color: ${prop => prop.backgroundColor}
-`;	
+	background-color: ${(prop) => prop.backgroundColor};
+`;
 
 const EventPicture = styled.img`
 	width: 100%;
@@ -23,54 +23,61 @@ const EventInfoContainer = styled.div`
 `;
 
 const EventDetail = styled.div`
-	width:100%;
+	width: 100%;
 	align-items: center;
 	display: grid;
-	grid-template-rows: repeat(2,1fr) auto;
+	grid-template-rows: repeat(2, 1fr) auto;
 	margin: 2%;
 `;
+
 const EventTitle = styled.div`
-	width:100%;
+	width: 100%;
 	font-style: Bold;
- 	text-align: left;
+	text-align: left;
 	font-size: 1.5rem;
-  	font-family: Al Bayan;
-  	font-weight: 700;
+	font-family: Al Bayan;
+	font-weight: 700;
 	color: rgba(37, 34, 34, 1);
 `;
+
 const EventTextContainer = styled.div`
 	width: 100%;
 	display: grid;
-	grid-template-columns: repeat(3,1fr);
+	grid-template-columns: repeat(3, 1fr);
 `;
+
 const EventDetailText = styled.div`
 	width: 100%;
 	height: auto;
 	display: flex;
 	flex-direction: column;
-	align-items:center;
+	align-items: center;
 `;
+
 const EventTextContainer2 = styled.div`
 	width: 100%;
 	height: auto;
 	display: grid;
 	flex-direction: column;
-	align-items:center;
+	align-items: center;
 	grid-template-columns: 1fr 1fr;
-`; 
-const JoinButton =styled.button`
+`;
+
+const JoinButton = styled(Link)`
 	width: 80%;
+	padding: 0.25rem;
 	display: flex;
+	justify-content: center;
 	text-align: center;
+	text-decoration: none;
 	border-radius: 0.5rem;
+	color: var(--primary-black);
 	font-family: Poppins;
 	font-weight: 500;
 	background-color: rgba(130, 174, 142, 1);
 `;
+
 const Event = (prop) => {
-	const style={
-        textDecoration: 'none'
-    };
 	return (
 		<EventBox backgroundColor={prop.backgroundColor}>
 			<EventInfoContainer>
@@ -78,9 +85,7 @@ const Event = (prop) => {
 				<EventDetail>
 					<EventTitle>{prop.eventName}</EventTitle>
 					<EventTextContainer>
-						<EventDetailText>
-							{prop.date}
-						</EventDetailText>
+						<EventDetailText>{prop.date}</EventDetailText>
 						<EventDetailText>{prop.time}</EventDetailText>
 						<EventDetailText>
 							{/* <EventPicture src="/assets/userIcon.svg" /> */}50+
@@ -89,15 +94,15 @@ const Event = (prop) => {
 					</EventTextContainer>
 				</EventDetail>
 				<EventTextContainer2>
-					<Link to="/RegisterPage" style={style}>
-						<JoinButton>Join now!</JoinButton>
-					</Link>
+					<JoinButton to="/RegisterPage">Join now!</JoinButton>
 					<EventDetailText>
-					{/* <EventPicture src="/assets/maptag.svg" /> */}{prop.location}
+						{/* <EventPicture src="/assets/maptag.svg" /> */}
+						{prop.location}
 					</EventDetailText>
 				</EventTextContainer2>
 			</EventInfoContainer>
 		</EventBox>
 	);
 };
+
 export default Event;
