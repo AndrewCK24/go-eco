0d765db1fb6b13cda45632f847475b05d9f49559
andrewhb614 <andrew50412@gmail.com>
2023-06-05 02:40:38 +0800

    新增套件 gh-pages 將網頁部署到 github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: process.env.NODE_ENV === "production" ? "/go-eco/" : "/",
 	plugins: [react(), svgr()],
 	resolve: {
 		alias: [
