035d9b1dff5d84e103b6694d36cd98d9909bb722
andrewhb614 <andrew50412@gmail.com>
2023-06-14 17:36:52 +0800

    修改 findEventById 新增除錯訊息以偵測 function 無法使用問題

--- netlify/functions/findEventById.js
+++ netlify/functions/findEventById.js
@@ -3,18 +3,23 @@ import Event from "../../models/event";
 
 export const handler = async (event) => {
 	try {
+		console.log('Connecting to MongoDB...');
 		connect(process.env.MONGODB_URI, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 		});
+		console.log('Connected to MongoDB.');
 		const { eventId } = JSON.parse(event.body);
+		console.log(`Finding event with ID ${eventId}...`);
 		const foundEvent = await Event.findById(eventId);
+		console.log(`Found event with ID ${eventId}: ${JSON.stringify(foundEvent)}`);
 
 		return {
 			statusCode: 200,
 			body: JSON.stringify(foundEvent),
 		};
 	} catch (error) {
+		console.error(`Error: ${error}`);
 		return {
 			statusCode: 500,
 			body: error.toString(),
