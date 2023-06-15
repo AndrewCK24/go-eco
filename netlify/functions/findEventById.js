0005cd4b313abcac4f7d8a3f6f2f9badeb3498d6
andrewhb614 <andrew50412@gmail.com>
2023-06-15 16:21:31 +0800

    修改 findEventById 將 schema 移入同檔案

--- netlify/functions/findEventById.js
+++ netlify/functions/findEventById.js
@@ -1,21 +1,96 @@
 import mongoose from "mongoose";
-import Event from "../../models/event";
+// import Event from "../../models/event";
+
+const eventSchema = new mongoose.Schema({
+	hostId: {
+		// ID of the user who created the event
+		type: String,
+		required: true,
+	},
+	timestamp: {
+		// Date of creation
+		type: Date,
+		required: true,
+	},
+	name: {
+		// Name of the event
+		type: String,
+		required: true,
+	},
+	eventDate: {
+		begDate: {
+			type: String,
+			required: true,
+		},
+		begTime: {
+			type: String,
+			required: true,
+		},
+		endDate: {
+			type: String,
+			required: true,
+		},
+		endTime: {
+			type: String,
+			required: true,
+		},
+	},
+	applyDate: {
+		endDate: {
+			type: String,
+			required: true,
+		},
+	},
+	type: {
+		type: String,
+		required: true,
+	},
+	introduction: {
+		brief: {
+			type: String,
+			required: true,
+		},
+		detail: {
+			type: String,
+			required: true,
+		},
+	},
+	location: {
+		name: {
+			type: String,
+			required: true,
+		},
+		address: {
+			type: String,
+			required: true,
+		},
+	},
+	volunteers: {
+		// List of volunteers' id
+		type: [String],
+		required: true,
+	},
+});
+
+const Event = mongoose.model("Event", eventSchema, "Event");
 
 export const handler = async (event) => {
 	if (!mongoose.connections[0].readyState) {
-		console.log('Connecting to MongoDB...');
+		console.log("Connecting to MongoDB...");
 		await mongoose.connect(process.env.MONGODB_URI, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 			bufferCommands: false,
 		});
-		console.log('Connected to MongoDB.');
+		console.log("Connected to MongoDB.");
 	}
 	try {
 		const { eventId } = JSON.parse(event.body);
 		console.log(`Finding event with ID ${eventId}...`);
 		const foundEvent = await Event.findById(eventId);
-		console.log(`Found event with ID ${eventId}: ${JSON.stringify(foundEvent)}`);
+		console.log(
+			`Found event with ID ${eventId}: ${JSON.stringify(foundEvent)}`
+		);
 
 		return {
 			statusCode: 200,
