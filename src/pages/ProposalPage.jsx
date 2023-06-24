2e754dee641f4122ed3ca00311b308d02716828c
andrewhb614 <andrew50412@gmail.com>
2023-06-24 11:32:56 +0800

    修改 ProposalPage 以 MUI 重構

--- src/pages/ProposalPage.jsx
+++ src/pages/ProposalPage.jsx
@@ -1,163 +1,74 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useRecoilValue } from "recoil";
 import styled from "@emotion/styled";
+import {
+	Button,
+	Checkbox,
+	Divider,
+	FormControl,
+	FormControlLabel,
+	FormGroup,
+	InputLabel,
+	TextField,
+	Select,
+	MenuItem,
+} from "@mui/material";
+import { DateCalendar, DatePicker, TimePicker } from "@mui/x-date-pickers";
+import dayjs from "dayjs";
+
+import userState from "../recoil/userState";
 
 const Container = styled.div`
 	width: 100%;
 	display: flex;
-	overflow: auto;
 	align-items: center;
 	flex-direction: column;
-	margin-bottom: 3%;
+	padding: 1.5rem 0;
 `;
 
 const ProposalContainer = styled.div`
 	width: 80%;
 	gap: 1rem;
-	top: 15%;
 	display: flex;
 	padding: 3rem;
-	border-radius: 2rem;
+	border-radius: 1rem;
 	border-style: solid;
-	border-color: green;
+	border-color: var(--bg-green-dark);
 	flex-direction: column;
 	background-color: white;
 `;
 
-const Line1 = styled.hr`
-	border-top: solid rgba(107, 188, 129, 1);
-	width: 23%;
-`;
-
-const Line2 = styled.hr`
-	border-top: solid rgba(180, 231, 194, 1);
-	width: 100%;
-`;
-
-const Row = styled.div`
-	color: rgba(0, 0, 0, 1);
-	text-align: left;
-	font-weight: 700;
+const DateTimeContainer = styled.div`
 	display: flex;
 	flex-direction: row;
-	gap: 2rem;
+	flex-wrap: wrap;
+	gap: 1rem;
+	align-items: stretch;
+	justify-content: center;
 `;
 
-const TextArea = styled.textarea`
-	border-color: rgba(0, 0, 0, 0.3);
-	border-style: solid;
-	border-width: 0.1rem;
-	border-radius: 0.5rem;
-	background-color: rgba(217, 217, 217, 0.3);
-	resize: none;
-	height: ${(props) => props.height};
-`;
-
-const DateTime = styled.input`
-	border-radius: 0.5rem;
-	width: 20%;
-	min-width: 9rem;
+const TimeContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 1rem;
 `;
 
 const Title = styled.div`
-	color: rgba(0, 0, 0, 1);
+	padding: 1rem 0;
+	color: var(--text-black);
 	height: auto;
 	font-size: 2rem;
 	text-align: center;
 	font-weight: 700;
 `;
 
-const LeftBlock = styled.label`
-	color: rgba(0, 0, 0, 1);
-	width: 13%;
-	font-size: 1rem;
-	text-align: left;
-	font-weight: 600;
-`;
-
-const RightBlock = styled.div`
-	color: rgba(0, 0, 0, 1);
-	display: flex;
-	flex-direction: column;
-	width: 85%;
-	text-align: left;
-	font-weight: 600;
-`;
-
-const DateTimeContainer = styled.div`
-	display: flex;
-	flex-direction: row;
-	gap: 2rem;
-`;
-
-const Text1 = styled.span`
-	color: rgba(0, 0, 0, 0.6499999761581421);
-	font-size: 1rem;
-	text-align: center;
-	font-family: Poppins;
-	font-style: normal;
-	font-weight: 500;
-	line-height: normal;
-	font-stretch: normal;
-	text-decoration: none;
-`;
-
-const Text2 = styled.div`
-	color: rgba(0, 0, 0, 0.5);
-	font-size: 0.8rem;
-	font-style: Medium;
-	text-align: left;
-	font-family: Poppins;
-	font-weight: 500;
-	line-height: normal;
-	font-stretch: normal;
-	text-decoration: none;
-`;
-
-const CheckBox = styled.input``;
-const CheckBoxLabel = styled.label``;
-const Button = styled.button`
-	border-color: rgba(89, 151, 106, 1);
-	border-style: solid;
-	border-width: 0.1rem;
-	border-radius: 0.3rem;
-	font-family: "Poppins";
-	font-style: normal;
-	font-weight: 500;
-	background-color: white;
-	color: rgba(89, 151, 106, 1);
-	padding: 0.7rem 1rem;
-`;
-
-const RowContainer = ({
-	leftText,
-	height,
-	setFormData,
-	formData,
-	name,
-	rightText,
-}) => {
-	const handleInputChange = (e) => {
-		setFormData({ ...formData, [name]: e.target.value });
-	};
-
-	return (
-		<Row>
-			<LeftBlock>{leftText}</LeftBlock>
-			<RightBlock>
-				<TextArea required height={height} onChange={handleInputChange} />
-				<Text2>{rightText}</Text2>
-			</RightBlock>
-		</Row>
-	);
-};
-
 const ProposalPage = () => {
 	const [formData, setFormData] = useState({});
-	// const setSavedEvent = useSetRecoilState(savedEventState);
+	const user = useRecoilValue(userState);
 	const navigate = useNavigate();
 	const handleConfirm = async (formData) => {
-		const hostId = "HOST_USER_ID";
+		const hostId = user._id;
 		const timestamp = Date.now();
 		const {
 			name,
@@ -209,156 +120,165 @@ const ProposalPage = () => {
 		}
 	};
 
-	// TODO: 表單必填要求、時間先後驗證、表單重構
 	return (
 		<Container>
-			<Title>Create your proposal</Title>
-			<Line1 />
-			<br />
-			<br />
+			<Title>建立你的活動</Title>
 			<ProposalContainer>
-				<Row>
-					<LeftBlock>開始時間</LeftBlock>
-					<RightBlock>
-						<DateTimeContainer>
-							<DateTime
-								type="date"
-								onChange={(e) => {
-									setFormData({ ...formData, begDate: e.target.value });
-								}}
-							/>
-							<DateTime
-								type="time"
-								onChange={(e) => {
-									setFormData({ ...formData, begTime: e.target.value });
-								}}
-							/>
-						</DateTimeContainer>
-					</RightBlock>
-				</Row>
-				<Row>
-					<LeftBlock>結束時間</LeftBlock>
-					<RightBlock>
-						<DateTimeContainer>
-							<DateTime
-								type="date"
-								onChange={(e) => {
-									setFormData({ ...formData, endDate: e.target.value });
-								}}
-							/>
-							<DateTime
-								type="time"
-								onChange={(e) => {
-									setFormData({ ...formData, endTime: e.target.value });
-								}}
-							/>
-						</DateTimeContainer>
-					</RightBlock>
-				</Row>
-				<Line2 />
-				<Row>
-					<LeftBlock>報名截止日</LeftBlock>
-					<RightBlock>
-						<DateTime
-							type="date"
-							onChange={(e) => {
-								setFormData({ ...formData, applyEndDate: e.target.value });
+				<DateTimeContainer>
+					<DateCalendar
+						showDaysOutsideCurrentMonth
+						disablePast
+						required
+						onChange={(date) => {
+							setFormData({
+								...formData,
+								begDate: dayjs(date).format("YYYY-MM-DD"),
+								endDate: dayjs(date).format("YYYY-MM-DD"),
+							});
+						}}
+					/>
+					<TimeContainer>
+						<TimePicker
+							label="活動開始時間"
+							required
+							onChange={(time) => {
+								setFormData({
+									...formData,
+									begTime: time.format("HH:mm"),
+								});
 							}}
 						/>
-					</RightBlock>
-				</Row>
-
-				<Line2 />
-				<Row>
-					<LeftBlock>活動種類</LeftBlock>
-					<RightBlock>
-						<select
-							id="type"
-							name="type"
-							onChange={(e) => {
-								setFormData({ ...formData, type: e.target.value });
-							}}
+						<TimePicker
+							label="活動結束時間"
 							required
-						>
-							<option value="">請選擇活動種類</option>
-							<option value="recycle">環保回收</option>
-							<option value="cleanUp">環境清理</option>
-						</select>
-					</RightBlock>
-				</Row>
-				<Line2 />
-				<RowContainer
-					leftText="活動名稱"
-					formData={formData}
-					setFormData={setFormData}
-					name="name"
+							onChange={(time) => {
+								setFormData({
+									...formData,
+									endTime: time.format("HH:mm"),
+								});
+							}}
+						/>
+					</TimeContainer>
+				</DateTimeContainer>
+				<Divider variant="middle" />
+				<DatePicker
+					id="outlined-basic"
+					name="applyEndDate"
+					label="報名截止日"
+					format="MM/DD/YYYY"
+					disablePast
+					required
+					value={formData.applyEndDate}
+					onChange={(e) => {
+						setFormData({
+							...formData,
+							applyEndDate: dayjs(e).format("YYYY-MM-DD"),
+						});
+					}}
 				/>
-				<RowContainer
-					leftText="活動簡介"
-					formData={formData}
-					setFormData={setFormData}
+				<Divider variant="middle" />
+				<TextField
+					required
+					id="outlined-basic"
+					name="location"
+					label="活動地點"
+					variant="outlined"
+					onChange={(e) => {
+						setFormData({ ...formData, location: e.target.value });
+					}}
+				/>
+				<TextField
+					required
+					id="outlined-basic"
+					name="address"
+					label="地址"
+					variant="outlined"
+					onChange={(e) => {
+						setFormData({ ...formData, address: e.target.value });
+					}}
+				/>
+				<Divider variant="middle" />
+				<TextField
+					required
+					id="outlined-basic"
+					name="name"
+					label="活動名稱"
+					variant="outlined"
+					onChange={(e) => {
+						setFormData({ ...formData, name: e.target.value });
+					}}
+				/>
+				<FormControl required fullWidth>
+					<InputLabel>活動種類</InputLabel>
+					<Select
+						name="type"
+						id="type"
+						label="活動種類"
+						variant="outlined"
+						defaultValue=""
+						onChange={(e) => {
+							setFormData({ ...formData, type: e.target.value });
+						}}
+					>
+						<MenuItem value="recycle">環保回收</MenuItem>
+						<MenuItem value="cleanUp">環境清理</MenuItem>
+					</Select>
+				</FormControl>
+				<TextField
+					required
+					multiline
+					rows={4}
+					id="outlined-basic"
 					name="briefIntroduction"
-					height="5rem"
+					label="活動簡介"
+					variant="outlined"
+					onChange={(e) => {
+						setFormData({ ...formData, briefIntroduction: e.target.value });
+					}}
+				/>
+				<TextField
+					required
+					multiline
+					rows={10}
+					id="outlined-basic"
+					name="introduction"
+					label="活動內容"
+					variant="outlined"
+					helperText="Please tell us the story about your project and why 
+					people should support it. Please note: must have enough information 
+					to review the plan. If you provide too little information, or cannot 
+					evaluate the authenticity and feasibility of the plan, the plan will 
+					not be put on the shelves."
+					onChange={(e) => {
+						setFormData({ ...formData, introduction: e.target.value });
+					}}
 				/>
 				{/* <RowContainer
-					leftText="Cover Photo"
-					rightText="Please upload an image smaller than 1MB. The recommended size is
-            1200 x 675 pixels (16:9). The cover image can be edited and modified
-            before the project goes live."
-				/> */}
-				<RowContainer
-					leftText="活動詳情"
-					formData={formData}
-					setFormData={setFormData}
-					name="introduction"
-					height="20rem"
-					rightText="Please tell us the story about your project and why people should
-            support it. Please note: tsk tsk must have enough
-            information to review the plan. If you provide too little
-            information, or tsk tsk cannot evaluate the authenticity and
-            feasibility of the plan, the plan will not be put on the shelves."
-				/>
-				<Line2 />
-				<RowContainer
-					leftText="活動地點"
-					formData={formData}
-					setFormData={setFormData}
-					name="location"
-				/>
-				<RowContainer
 					leftText="地址"
 					formData={formData}
 					setFormData={setFormData}
 					name="address"
-				/>
-				<Line2 />
-
-				<Row>
-					<LeftBlock>Terms of cooperation</LeftBlock>
-					<RightBlock>
-						<CheckBoxLabel>
-							<CheckBox type="checkbox" />
-							<Text1>
-								&emsp;The proposer (and the legal representative) have fully
+				/> */}
+				<Divider variant="middle" />
+				<FormGroup>
+					<FormControlLabel
+						required
+						control={<Checkbox />}
+						label="The proposer (and the legal representative) have fully
 								understood and agreed to the rights and obligations of the terms
-								of cooperation
-							</Text1>
-						</CheckBoxLabel>
-						<Text2>
-							Please confirm that you have read and agree to the terms of
-							cooperation of Gee x Proposer ..
-						</Text2>
-					</RightBlock>
-				</Row>
+								of cooperation"
+					/>
+				</FormGroup>
+				<Button
+					variant="contained"
+					color="success"
+					onClick={() => {
+						handleConfirm(formData);
+					}}
+				>
+					CONFIRM
+				</Button>
 			</ProposalContainer>
-			<br />
-			<Button
-				onClick={() => {
-					handleConfirm(formData);
-				}}
-			>
-				CONFIRM
-			</Button>
 		</Container>
 	);
 };
